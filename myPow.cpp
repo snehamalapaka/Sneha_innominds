a4a12dfc06d9c0c1085199b70b585cf7839a3c86
snehamalapaka <sneha2003apr@gmail.com>
2025-01-22 10:29:51 +0530

    Update myPow.cpp

--- myPow.cpp
+++ myPow.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 using namespace std;
 
-//worked on online gdb
+//lest take 3^5 here we write 5 in binary which is 101
+//here x =3,n=5
 
 int myPow(double x,int n) {
     //edge cases
@@ -10,7 +11,7 @@ int myPow(double x,int n) {
     if(x==1) return 1.0;
     if(x==-1 && n%2 ==0) return 1.0;
 
-    long binForm =n;
+    long binForm =n;//if power is neagtive 
     if(n<0) {
         x=1/x;
         binForm=-binForm;
@@ -20,12 +21,13 @@ int myPow(double x,int n) {
 
     while (binForm>0)
     {
-       if(binForm%2==1) {
-        ans =ans*x;
+       if(binForm%2==1) {//to check if binary form has 0 or 1 in ending
+        ans =ans*x;       //if binary form has 1 , we need to multiply ans with x
+                        // and also return x^2 so that we need not calculate again if we get 1 in later iterations of bin form
 
        }
        x=x*x;
-       binForm/=2;
+       binForm/=2;    
 
     }
     return ans;
