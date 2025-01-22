d340100896f8ba38f877b26294e84169a5e1bbaa
snehamalapaka <sneha2003apr@gmail.com>
2025-01-22 10:53:46 +0530

    Update stockbuysell.cpp

--- stockbuysell.cpp
+++ stockbuysell.cpp
@@ -2,11 +2,14 @@
 #include <vector>
 using namespace std;
 
-//worked on online gdb
+//to calc max profit from stocks if given array of prices, idx is the day
+//lets assume everyday as selling day
+//as we cant sell bedore we buy lets start from day 1 not day 0
 
 int stockbuysell(vector<int>prices) {
     int maxProfit =0, bestBuy = prices[0];
-    for(int i=1;i<prices.size();i++) {
+    //loop from day i to calc max profit if we sell on day i, i starts from day 1
+    for(int i=1;i<prices.size();i++) { //given array of prices
         if(prices[i]>bestBuy){
             maxProfit=max(maxProfit,prices[i]-bestBuy);
         }
