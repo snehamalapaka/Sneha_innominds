de33f8648c7bf9456978ae2d444023288481a5a0
snehamalapaka <sneha2003apr@gmail.com>
2025-01-22 10:39:55 +0530

    Update saerchin2Darray.cpp

--- saerchin2Darray.cpp
+++ saerchin2Darray.cpp
@@ -2,7 +2,8 @@
 #include <vector>
 using namespace std;
 
-//code worked on online gdb
+//to search target element in 2D array using binary search algorithm
+//given elements are arranged in non decreasing order
 
 bool searchInRow(vector<vector<int>>&mat, int target ,int row) {
     //n rows m colums 
@@ -10,16 +11,16 @@ bool searchInRow(vector<vector<int>>&mat, int target ,int row) {
     int n =mat[0].size();
     int st =0, end=n-1;
 
-    //binary search in row
+    //binary search with in row
     while(st<=end) {
         int mid=st + (end-st)/2;
         if(target==mat[row][mid]){
-            return true;
-        }else if (target>mat[row][mid]){
+            return true;//target here is in mid itself
+        }else if (target>mat[row][mid]){ //target greater than mid so go to right half
             st = mid+1;
 
         }else {
-            end=mid-1;
+            end=mid-1; //target less tham mid so search in left half
         }
     }
     return false;
@@ -35,9 +36,10 @@ bool searchMatrix(vector<vector<int>>& mat,int target){
         if(target>=mat[midRow][0]&& target<=mat[midRow][n-1]) {
              //found element in mid row
              return searchInRow(mat,target,midRow);
+            //search in next row / bottom row as target is larger than last element of middle row
         }else if (target >= mat[midRow][n-1]) {
             StartRow=midRow+1;
-        }else {
+        }else { //search element is upper row as target is less than first elment of middle row
             endRow=midRow-1;
         }
        
