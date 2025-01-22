1cdcbd66022d0925add1901f2400f3a3b65b0b64
snehamalapaka <sneha2003apr@gmail.com>
2025-01-22 10:25:54 +0530

    Update maximumwater.cpp

--- maximumwater.cpp
+++ maximumwater.cpp
@@ -2,20 +2,22 @@
 #include<vector>
 using namespace std;
 
-//worked on online gdb
+//given a vector of length that is  height of water
+//the index of array between two heights is considered as width
+//the smallest of 2 heights is considered while calculating area
  
 int maximumwater(vector<int>height) {
     
     int maxWater = 0; //ans
-    int lp=0, rp=height.size()-1; //right and left pointers
+    int lp=0, rp=height.size()-1; //right and left pointers for height
    
    while(lp < rp) {
     int w= rp-lp; //width
-    int ht= min(height[lp],height[rp]);
+    int ht= min(height[lp],height[rp]);//small height is considered to avoid overflow
     int currWater = w*ht;
     maxWater = max(maxWater,currWater);
 
-    height[lp] < height[rp] ? lp++ : rp--;
+    height[lp] < height[rp] ? lp++ : rp--;//always move the smallest height pointer 
    }
    return maxWater;
 }
