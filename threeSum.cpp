e808740bcd65442b2cfee8a1796aeb3b99dcfb2d
snehamalapaka <sneha2003apr@gmail.com>
2025-01-22 11:04:25 +0530

    Update threeSum.cpp

--- threeSum.cpp
+++ threeSum.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-//why vector written twice? 
 
-//error in line 37
+
+// to find if some of three elements in array equals zero
 vector<vector<int>> threeSum(vector<int>nums) {
     int n =nums.size();
     vector<vector<int>>ans;
@@ -13,13 +13,15 @@ vector<vector<int>> threeSum(vector<int>nums) {
         if(i>0 && nums[i]==nums[i-1]) continue;
 
         int j =i+1, k=n-1;
-
+        //i points to 0th idx, j points to first idx and k points to last idx initially
+        // we put i fixed and try to get other 2 elements by moving j and k pointers to make triplet with i so that sum is 0
+        //in one iteration i is fixed , j and k move
         while (j < k)
         {
             int sum = nums[i]+nums[j]+nums[k];
             if(sum<0) {
-                j++;
-            }else if(sum>0) {
+                j++;  // if sum<0 to inc sum we should move j to right
+            }else if(sum>0) { //if sum>0 we should dec sum so move k to left
                 k--;
             }else {
                 ans.push_back({nums[i],nums[j],nums[k]});
